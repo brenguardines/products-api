da319a312f24b314896f540f0e1856c34de70ff0
Brenda Guardines <bguardines@gmail.com>
2023-01-24 14:10:36 -0300

    Subo faker refactorizado

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -5,12 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,23 +21,7 @@ public class ProductController {
 	
 	@GetMapping
 	public ResponseEntity<List<Product>> getProducts(){ //Lista con todos los productos
-		return new ResponseEntity<List<Product>>(service.getProducts(),HttpStatus.OK);
-	}
-	
-	@PostMapping
-	public ResponseEntity<Product> createProducts(@RequestBody Product product){ //Para crear un producto
-		return new ResponseEntity<Product>(service.createProduct(product), HttpStatus.CREATED);
-	}
-	
-	@PutMapping("/{productId}")
-	public ResponseEntity<Product> updateProducts(@PathVariable("productId")Integer productId, @RequestBody Product product){ //Para modificar un producto
-		return new ResponseEntity<Product>(service.updateProduct(productId, product), HttpStatus.OK);
-	}
-	
-	@DeleteMapping("/{productId}")
-	public ResponseEntity<Void> deleteProduct(@PathVariable("productId")Integer productId){ //Para eliminar un producto
-		service.deleteProduct(productId);
-		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		return new ResponseEntity<>(service.getProducts(),HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/example/demo/services/ProductService.java
+++ src/main/java/com/example/demo/services/ProductService.java
@@ -1,12 +1,9 @@
 package com.example.demo.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.entities.Product;
 import com.example.demo.repositories.ProductRepository;
@@ -19,34 +16,6 @@ public class ProductService {
 	
 	public List<Product> getProducts(){ //repositorio que devuelve todos los roles
 		return repository.findAll();
-	}
-	
-	public Product createProduct(Product product) {
-		return repository.save(product);
-	}
-	
-	public Product updateProduct(Integer productId, Product product) {
-		Optional<Product> result = repository.findById(productId);
-		if(result.isPresent()) {
-			Product savedProduct = result.get();
-			savedProduct.setName(product.getName());
-			savedProduct.setImageUrl(product.getImageUrl());
-			savedProduct.setPrice(product.getPrice());
-			return repository.save(savedProduct);
-		}else {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Product id %d doesn't exists", productId));
-		}
-	}
-
-	public void deleteProduct(Integer productId) {
-		Optional<Product> result = repository.findById(productId);
-		
-		if(result.isPresent()) {
-			repository.delete(result.get());
-		}else {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Product id %d doesn't exists", productId));
-		}
-		
 	}
 
 }
