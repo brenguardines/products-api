d60de9daa7056c685d3ac5b187770ffba58e1e89
Brenda Guardines <bguardines@gmail.com>
2023-01-24 14:04:34 -0300

    Subo faker

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,4 +44,5 @@ public class ProductController {
 		service.deleteProduct(productId);
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
+
 }
