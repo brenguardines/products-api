622a77b02eb513b25307fb9e5eee3354605c3351
Brenda Guardines <bguardines@gmail.com>
2023-01-25 11:41:37 -0300

    Subo faker paginado

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -1,12 +1,13 @@
 package com.example.demo.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Product;
@@ -20,8 +21,25 @@ public class ProductController {
 	private ProductService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Product>> getProducts(){ //Lista con todos los productos
-		return new ResponseEntity<>(service.getProducts(),HttpStatus.OK);
+	public ResponseEntity<Page<Product>> getProducts(@RequestParam(required = false, value = "page", defaultValue = "0") int page,
+			@RequestParam(required = false, value = "size", defaultValue = "100") int size){ //el required es para que se pueda acceder directo desde /products y no tener que hacer si o si /products?page=0&size=100
+		return new ResponseEntity<>(service.getProducts(page, size),HttpStatus.OK);
+	}
+	
+	@GetMapping("/names")
+	public ResponseEntity<Page<String>> getNames(@RequestParam(required = false, value = "page", defaultValue = "0") int page,
+			@RequestParam(required = false, value = "size", defaultValue = "100") int size){ //devuelve todos los nombres
+		return new ResponseEntity<>(service.getNames(page, size),HttpStatus.OK);
+	}
+	
+	@GetMapping("/{productId}")
+	public ResponseEntity<Product> getProductById(@PathVariable("productId") Integer productId){
+		return new ResponseEntity<>(service.getProductById(productId),HttpStatus.OK);
+	}
+	
+	@GetMapping("/name/{name}")
+	public ResponseEntity<Product> getProductByName(@PathVariable("name") String name){
+		return new ResponseEntity<>(service.getProductByName(name),HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/example/demo/services/ProductService.java
+++ src/main/java/com/example/demo/services/ProductService.java
@@ -1,9 +1,12 @@
 package com.example.demo.services;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.entities.Product;
 import com.example.demo.repositories.ProductRepository;
@@ -12,10 +15,24 @@ import com.example.demo.repositories.ProductRepository;
 public class ProductService {
 	
 	@Autowired //Para que lo inyecte
-	private ProductRepository repository;
+	private ProductRepository productRepository;
 	
-	public List<Product> getProducts(){ //repositorio que devuelve todos los roles
-		return repository.findAll();
+	public Page<Product> getProducts(int page, int size){ //page: numero de pagina y size: tamaño de pagina
+		 return productRepository.findAll(PageRequest.of(page, size));
+	}
+	
+	public Page<String> getNames(int page, int size){
+		return productRepository.findNames(PageRequest.of(page, size));
+	}
+	
+	public Product getProductById(Integer productId) {
+		return productRepository.findById(productId).orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND ,String.format("Product %d not found", productId)));		        
+	}
+	
+	public Product getProductByName(String name) {
+		return productRepository.findByName(name).orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND ,String.format("Product %d not found", name)));
 	}
 
 }
